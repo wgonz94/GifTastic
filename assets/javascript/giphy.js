ee61d7bdad72085123db823f8a527483e06cbd24
William Gonzales <wgonz94@yahoo.com>
2019-08-30 19:04:17 -0700

    able to pull data from api via button click event. Had to create function and put in a document listner for class movie.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,60 +1,87 @@
 
-    var topics = ["George of the Jungle", "Spiderman", "The Dark Knight", "The Mask", "Anchorman", "Blades of Glory", "The Matrix", "Old School", "Sherlock Holmes", "StarWars", "Benchwarmers", "Austin Powers", "Teenage Mutant Ninja Turtles"]
+var topics = ["George of the Jungle", "Spiderman", "The Dark Knight", "The Mask", "Anchorman", "Blades of Glory", "The Matrix", "Old School", "Sherlock Holmes", "StarWars", "Benchwarmers", "Austin Powers", "Teenage Mutant Ninja Turtles"];
 
-    function renderButtons() {
+function createButtons() {
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise we will have repeat buttons)
-        $("#buttons-view").empty();
+    // Deleting the movies prior to adding new movies
+    // (this is necessary otherwise we will have repeat buttons)
+    $("#buttons-view").empty();
 
-        // Looping through the array of movies
-        for (var i = 0; i < topics.length; i++) {
+    // Looping through the array of movies
+    for (var i = 0; i < topics.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-          var button = $("<button>");
-          // Adding a class of movie to button
-          button.addClass("movie");
-          button.addClass("btn-info");
-          button.addClass("m-1");
+        // Then dynamicaly generating buttons for each movie in the array
+        var button = $("<button>");
+        // Adding multiple classes movie div to button
+        button.addClass("movie");
+        // button.addClass("btn-info");
+        // button.addClass("m-1");
 
 
-          // Adding a data-attribute
-          button.attr("data-name", topics[i]);
-          // Providing the initial button text
-          button.text(topics[i]);
-          // Adding the button to the HTML
-          $("#buttons-view").append(button);
-        };
-      };
+        // Adding a data-attribute
+        button.attr("data-name", topics[i]);
+        // Providing the initial button text
+        button.text(topics[i]);
+        // Adding the button to the HTML
+        $("#buttons-view").append(button);
+    };
+};
 
-      // This function handles events where one button is clicked
-      $("#add-movie").on("click", function(event) {
-        // Preventing the buttons default behavior when clicked (which is submitting a form)
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var movie = $("#movie-input").val().trim();
+// This function handles events where one button is clicked
+$("#addMovie").on("click", function (event) {
+    // Preventing the buttons default behavior when clicked (which is submitting a form)
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var movie = $("#movie-input").val().trim();
 
-        // Adding the movie from the textbox to our array
-        topics.push(movie);
+    // Adding the movie from the textbox to our array
+    topics.push(movie);
 
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
+    // Calling renderButtons which handles the processing of our movie array
+    createButtons();
 
-      });
+});
 
-    // // Here we construct our URL
-    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    // movie + "&api_key=zBVYqCxlWQzucELEgm9SniwzfKORwVmQ&limit=10";
+
+function displayTopicGif() {
+// $("<button>").on("click", function () {
+    //able to generate api response with value entry for add movie button
+    // var name = $(movie-input).val().trim();
+
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=zBVYqCxlWQzucELEgm9SniwzfKORwVmQ";
+
+    //Need to figure out way to enact response with buttons displayed
+    var name = $(this).attr("data-name");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=zBVYqCxlWQzucELEgm9SniwzfKORwVmQ";
     
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "GET"
-    // }).then(function(response) {
-    //   $("#movie-view").text("Rated: " + JSON.stringify(response.Rated));
-    // });
-        
 
-  
+    // Performing AJAX GET request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
 
-  renderButtons();
+            console.log(response)
+
+        });
+    }
+// });
+// $(".gif").on("click", function() {
+//     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+//     var state = $(this).attr("data-state");
+//     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+//     // Then, set the image's data-state to animate
+//     // Else set src to the data-still value
+//     if (state === "still") {
+//       $(this).attr("src", $(this).attr("data-animate"));
+//       $(this).attr("data-state", "animate");
+//     } else {
+//       $(this).attr("src", $(this).attr("data-still"));
+//       $(this).attr("data-state", "still");
+//     }
+//   });
+
+//add listener for elements with class of "movie" *test run*
+$(document).on("click", ".movie", displayTopicGif);
+createButtons()
